faee0736a39affe62e7e545914de8d2f06624ce5
mrjafari202 <m.r.jafari202@gmail.com>
2023-12-06 01:56:08 -0800

    tranaction added!

--- app/components/profile/Sidebar.tsx
+++ app/components/profile/Sidebar.tsx
@@ -1,32 +1,12 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import Link from "next/link";
 
 
 const sidebar = () => {
   const pathName = usePathname()
 
   return (
-<<<<<<< HEAD
-    <div className="w-[266px] h-full rounded-md shadow-xl bg-[#F0F0F0] ">
-    <div className='w-[80%] flex flex-col mx-auto divide-y divide-[#D3D3D3] gap-y-4 py-[30px] '>
-    <Link href={'/profile/travels'}>
-        <h2 className="font-kalameh500 text-[12px] text-right mr-10 ">سفرهای من</h2>
-        <p className="font-kalameh400 text-[8px] text-right mr-10">لیست سفرها و درخواست ها </p>
-    </Link>
-
-    <Link href={'/profile/favorite'} className=" pt-4 ">
-        <h2 className="font-kalameh500 text-[12px] text-right mr-10"> موردعلاقه ها</h2>
-        <p className="font-kalameh400 text-[8px]  text-right mr-10">لیست اقامتگاه های مورد علاقه </p>
-    </Link>
-
-    <Link href={'/profile/transaction'}  className=" pt-4">
-        <h2 className="font-kalameh500 text-[12px] text-right mr-10"> تراکنش های من</h2>
-        <p className="font-kalameh400 text-[8px]  text-right mr-10">مشاهده تاریخ و تراکنش های شما</p>
-    </Link>
-    </div>
-=======
     <div className="w-[80%] mx-auto h-full rounded-md shadow-xl bg-[#F0F0F0] md:w-[25%] ">
       <div className='w-[80%] flex flex-col mx-auto divide-y divide-[#D3D3D3] gap-y-4 py-[30px] md:mx-auto '>
         <div>
@@ -50,7 +30,6 @@ const sidebar = () => {
           </Link>
         </div>
       </div>
->>>>>>> main
 
     </div>
   )
