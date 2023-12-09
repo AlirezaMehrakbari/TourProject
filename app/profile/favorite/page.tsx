e753895f7432864ae2b355481481f81ce25176e9
mrjafari202 <m.r.jafari202@gmail.com>
2023-12-08 23:25:09 -0800

    working profile responsive

--- app/profile/favorite/page.tsx
+++ app/profile/favorite/page.tsx
@@ -107,57 +107,61 @@ const Favorite = () => {
             <ProfileNavbar />
             </div>
 
-            <h1 className="font-kalameh700 mr-[295px]">مورد علاقه ها</h1>
-            <div className="w-full flex flex-row-reverse justify-end pb-10 pt-7">
 
-                <div className="w-[61%] h-[100%] mr-5 flex flex-row flex-wrap justify-around rounded-md 
-                 ">
-                    {
-                        villa.map((item) => (
-                            <div className="w-[220.595] flex flex-col text-[#706E6E] hover:text-[#000]">
-                                <Image
-                                    className='rounded-md w-[219px] max-h-[160px] object-cover object-center shadow-md hover:shadow-lg cursor-pointer '
-                                    src={item.image}
-                                    alt={item.title}
-                                />
-                                <div className="flex flex-row justify-between py-2 px-[2.98px]">
-                                    <div className="flex flex-row">
-                                        <p className="text-[9.85px]">{item.opinion}</p>
-                                        <p className="text-[9.85px] px-2">دیدگاه</p>
-                                     </div>
-                                    <div className="flex flex-row items-center">
-                                        <p className="text-[9.85px] px-1">
-                                            {item.Satisfaction}
-                                        </p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                                            <path d="M5.8182 1.0918L7.1999 4.03519L10.2897 4.51009L8.05396 6.79992L8.58159 10.0348L5.8182 8.50671L3.0548 10.0348L3.58244 6.79992L1.34668 4.51009L4.4365 4.03519L5.8182 1.0918Z" fill="#FFF500" stroke="#FFF500" stroke-width="1.1924" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
+            <div className="w-full flex flex-col justify-between pb-10 pt-7 md:flex-row gap-x-8">
+                <Sidebar />
+
+                <div className='w-full mx-auto flex flex-col rounded-md  md:mt-[-38px]'>
+                    <h1 className="font-kalameh700 pb-12 max-md:pt-10 max-md:text-center">مورد علاقه ها</h1>
+                    <div className="w-full mx-auto flex flex-col items-center rounded-md justify-between md:mt-[-2rem] md:flex md:flex-row md:flex-wrap md:w-[100%]">
+
+                        {
+                            villa.map((item) => (
+                                <div className="w-[60%] flex flex-col text-[#706E6E] hover:text-[#000] md:w-[28%] px-1 ">
+                                    <Image
+                                        className='rounded-md w-full max-h-[160px] object-cover object-center shadow-md hover:shadow-lg cursor-pointer '
+                                        src={item.image}
+                                        alt={item.title}
+                                    />
+                                    <div className="flex flex-row justify-between py-2 px-[2.98px]">
+                                        <div className="flex flex-row justify-center">
+                                            <p className="text-[9.85px]">{item.opinion}</p>
+                                            <p className="text-[9.85px] px-2">دیدگاه</p>
+                                        </div>
+                                        <div className="flex flex-row items-center">
+                                            <p className="text-[9.85px] px-1">
+                                                {item.Satisfaction}
+                                            </p>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+                                                <path d="M5.8182 1.0918L7.1999 4.03519L10.2897 4.51009L8.05396 6.79992L8.58159 10.0348L5.8182 8.50671L3.0548 10.0348L3.58244 6.79992L1.34668 4.51009L4.4365 4.03519L5.8182 1.0918Z" fill="#FFF500" stroke="#FFF500" stroke-width="1.1924" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                        </div>
+
+                                    </div>
+                                    <h1 className='font-kalameh500 text-[13.72px] text-[#000] px-[2.98px]'>{item.title} </h1>
+                                    <div className="flex flex-row py-2 px-[2.98px]">
+                                        <CiLocationOn />
+                                        <p className="px-1 text-[8.372px] ">{item.province} | </p>
+                                        <p className="px-1 text-[8.372px] "> {item.city}</p>
                                     </div>
 
-                                </div>
-                                <h1 className='font-kalameh500 text-[13.72px] text-[#000] px-[2.98px]'>{item.title} </h1>
-                                <div className="flex flex-row py-2 px-[2.98px]">
-                                    <CiLocationOn />
-                                    <p className="px-1 text-[8.372px] ">{item.province} | </p>
-                                    <p className="px-1 text-[8.372px] "> {item.city}</p>
-                                </div>
+                                    <div className="flex flex-row justify-between pb-[52px] px-[2.98px]">
+                                        <div className="flex flex-row">
+                                            <TfiMoney />
+                                            <p className="text-[8.372px]">قیمت برای هر شب</p>
+                                        </div>
 
-                                <div className="flex flex-row justify-between pb-[52px] px-[2.98px]">
-                                    <div className="flex flex-row">
-                                        <TfiMoney />
-                                        <p className="text-[8.372px]">قیمت برای هر شب</p>
-                                    </div>
-
-                                    <div className="flex flex-row">
-                                        <p className="text-[8.372px] font-kalameh400 ">{item.price} تومان</p>
+                                        <div className="flex flex-row">
+                                            <p className="text-[8.372px] font-kalameh400 ">{item.price} تومان</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
+
                 </div>
 
-                <Sidebar />
 
             </div>
 
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,73 +3,67 @@ import Button from "@/app/components/Button";
 import Sidebar from "../components/profile/Sidebar";
 import ProfileNavbar from "../components/profile/ProfileNavbar";
 
-
-type Option = {
-    value: string,
-    label: string
-}
 const ProfilePage = () => {
-    const options: Option[] = [
-        { value: 'اطلاعات حساب کاربری', label: 'اطلاعات حساب کاربری' },
-        { value: 'خروج از حساب کاربری', label: 'خروج از حساب کاربری' }
-    ]
+
     return (
-        <div className='w-[90%] mx-auto flex flex-col'>
+        <div className='w-[90%] flex flex-col'>
             <div className="py-8">
                 <ProfileNavbar />
             </div>
 
-            <h1 className="font-kalameh700 mr-[338px]">حساب کاربری</h1>
-            <div className="w-[full]  flex flex-row-reverse justify-between pb-10 pt-7">
-
-                <div className="w-[70%] mr-[70px] mx-auto flex flex-col rounded-md shadow-2xl bg-[#F8F8F8] py-8">
-
-                    <form className="px-3">
-                        <div className="flex">
-                            <div className="relative">
-                                <div className="px-10">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
-                                    <circle cx="28" cy="28" r="28" fill="#D9D9D9" />
-                                </svg>
-                                </div>
-                                <div className="absolute bottom-0 left-10">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
-                                        <circle cx="6.5" cy="6.5" r="5.91667" fill="#A0A0A0" stroke="#A0A0A0" stroke-width="1.16667" />
-                                    </svg>
-                                    <div className="absolute inset-0 flex justify-center items-center text-[10px] rounded-full text-white">+</div> 
-                                </div>
-                            </div>
-                            <h3 className="px-[36px] text-[15px] font-kalameh400 self-center">افزودن عکس پروفایل</h3>
-                        </div>
-                        <div className="flex flex-row border-t border-[#B6B6B6] mt-5">
-                            <p className="font-kalameh400 text-[12px] mr-10  my-10">نام و نام خانوادگی</p>
-                            <input className="w-1/4 h-8 rounded-md border border-solid mx-2 bg-transparent text-center  my-8 mr-11" type="text" placeholder="نام" />
-                            <input className="w-1/4 h-8 rounded-md border border-solid bg-transparent text-center  my-8  mr-0" type="text" placeholder="نام خانوادگی" />
-                        </div>
-                        <div className="flex flex-row">
-                            <p className="font-kalameh400 text-[12px] mr-10  my-10">کد ملی</p>
-                            <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[98px]" type="text" />
-                        </div>
-                        <div className="flex flex-row">
-                            <p className="font-kalameh400 text-[12px] mr-10  my-10">تاریخ تولد</p>
-                            <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[88px] " type="number" />
-                        </div>
-                        <div className="flex flex-row">
-                            <p className="font-kalameh400 text-[12px] mr-10 my-10">شماره همراه</p>
-                            <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[70px]" type="number" />
-                        </div>
-                        <div className="flex flex-row pt-7 mr-[168px]">
-                            <div className="mx-1">
-                               <Button styles={'w-[100px] rounded-md bg-[#000]'} >ثبت</Button>
-                            </div>
-                            <div className="mx-1">
-                                <Button outline styles={'w-[100px] rounded-md bg-[#B1B1B1] border-none text-white '}>انصراف</Button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
+            <div className="w-full flex flex-col justify-evenly pb-10 pt-7 md:flex-row gap-x-8">
                 <Sidebar />
+                   <div>
+                       <h1 className="font-kalameh700 pb-12 max-md:pt-10 max-md:text-center">حساب کاربری</h1>
+                       <div className="w-full mx-auto flex flex-col rounded-md shadow-2xl bg-[#F8F8F8] py-8 md:mt-[-38px]">
+
+                           <form className="px-3">
+                               <div className="flex">
+                                   <div className="relative">
+                                       <div className="px-10">
+                                           <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
+                                               <circle cx="28" cy="28" r="28" fill="#D9D9D9" />
+                                           </svg>
+                                       </div>
+                                       <div className="absolute bottom-0 left-10">
+                                           <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+                                               <circle cx="6.5" cy="6.5" r="5.91667" fill="#A0A0A0" stroke="#A0A0A0" stroke-width="1.16667" />
+                                           </svg>
+                                           <div className="absolute inset-0 flex justify-center items-center text-[10px] rounded-full text-white">+</div>
+                                       </div>
+                                   </div>
+                                   <h3 className="px-[36px] text-[15px] font-kalameh400 self-center">افزودن عکس پروفایل</h3>
+                               </div>
+                               <div className="flex flex-row border-t border-[#B6B6B6] mt-5">
+                                   <p className="font-kalameh400 text-[12px] mr-10  my-10">نام و نام خانوادگی</p>
+                                   <input className="w-1/4 h-8 rounded-md border border-solid mx-2 bg-transparent text-center  my-8 mr-11" type="text" placeholder="نام" />
+                                   <input className="w-1/4 h-8 rounded-md border border-solid bg-transparent text-center  my-8  mr-0" type="text" placeholder="نام خانوادگی" />
+                               </div>
+                               <div className="flex flex-row">
+                                   <p className="font-kalameh400 text-[12px] mr-10  my-10">کد ملی</p>
+                                   <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[98px]" type="text" />
+                               </div>
+                               <div className="flex flex-row">
+                                   <p className="font-kalameh400 text-[12px] mr-10  my-10">تاریخ تولد</p>
+                                   <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[88px] " type="number" />
+                               </div>
+                               <div className="flex flex-row">
+                                   <p className="font-kalameh400 text-[12px] mr-10 my-10">شماره همراه</p>
+                                   <input className="w-[51%] h-8 rounded-md border border-solid mx-2 bg-transparent text-center my-8 mr-[70px]" type="number" />
+                               </div>
+                               <div className="flex flex-row pt-7 mr-[168px]">
+                                   <div className="mx-1">
+                                       <Button styles={'w-[100px] rounded-md bg-[#000]'} >ثبت</Button>
+                                   </div>
+                                   <div className="mx-1">
+                                       <Button outline styles={'w-[100px] rounded-md bg-[#B1B1B1] border-none text-white '}>انصراف</Button>
+                                   </div>
+                               </div>
+                           </form>
+                       </div>
+
+                   </div>
+
 
             </div>
 
