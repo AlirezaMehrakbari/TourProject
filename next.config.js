56d53492c257e064eab769a8a30569d03c7c6645
AlirezaMehrakbari <alirezamehrakbari@gmail.com>
2024-01-01 15:45:48 +0330

    sync

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swMinify: true,
-    disable: false,
-    workboxOptions: {
-        disableDevLogs: true
-    }
-});
 const nextConfig = {}
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
