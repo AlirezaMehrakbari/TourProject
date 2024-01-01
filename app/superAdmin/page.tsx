4a8eccac53e0baa40c40b6344966a38c1be360dc
mrjafari202 <m.r.jafari202@gmail.com>
2024-01-01 08:59:12 -0800

    sync

--- app/superAdmin/page.tsx
+++ app/superAdmin/page.tsx
@@ -150,13 +150,13 @@ const SuperAdmin = () => {
                                     <div className="flex items-center mb-4 gap-x-2">
                                         <label
                                                className=" font-kalameh400 text-[24px] text-gray-900 dark:text-gray-300"> داخلی</label>
-                                        <input type="checkbox" value=""
+                                        <input type="radio" name='test'
                                                className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                                     </div>
                                     <div className="flex items-center mb-4 gap-x-2">
                                         <label
                                                className="font-kalameh400 text-[24px] text-gray-900 dark:text-gray-300"> خارجی</label>
-                                        <input type="checkbox" value=""
+                                        <input type="radio" name='test'
                                                className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                                     </div>
                                 </div>
