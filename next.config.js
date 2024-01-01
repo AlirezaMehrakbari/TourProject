749c47e47fb0a22f03cde6b515457f44fa30a07f
AlirezaMehrakbari <alirezamehrakbari@gmail.com>
2024-01-01 15:09:37 +0330

    Add PWA in Project

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const nextConfig = {
-    ...withPWA({
-        dest : 'public',
-        register : true,
-        skipWaiting : true
-    })
-}
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swMinify: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true
+    }
+});
+const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
